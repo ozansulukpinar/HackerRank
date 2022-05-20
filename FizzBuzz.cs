6b6b99a21e8186c867c89526f5f56d9da557b958
Ozan Sülükpınar <ozansulukpinar@hotmail.com>
2022-05-20 20:32:43 +0300

    Fizzbuzz algorithm completed

---
+++ FizzBuzz.cs
@@ -0,0 +1,34 @@
+using System;
+
+class Result
+{
+    public static void fizzBuzz(int n)
+    {
+        for(int j = 1; j <= n; j++ ){
+            if(j % 3 == 0){
+                if(j % 5 == 0){
+                    Console.WriteLine("FizzBuzz");
+                }
+                else{
+                    Console.WriteLine("Fizz");
+                }
+            }
+            
+            if(j % 5 == 0 && j % 3 != 0)
+                Console.WriteLine("Buzz");
+            
+            if(j % 5 != 0 && j % 3 != 0)
+                Console.WriteLine(j);
+        }
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        Result.fizzBuzz(n);
+    }
+}
